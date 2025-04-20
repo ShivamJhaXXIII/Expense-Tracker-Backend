1a98a369bd882ee2e9302a79cfd7ef7bb48b6853
ShivamJhaXXIII <shivam23040223@gmail.com>
2025-04-21 00:47:41 +0530

    Lombok is working now

--- src/main/java/com/shivam/ExpenseTracker/model/Expense.java
+++ src/main/java/com/shivam/ExpenseTracker/model/Expense.java
@@ -7,61 +7,18 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Expense {
-    public Expense(Long id, String title, String category, BigDecimal amount, LocalDate date) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
-        this.amount = amount;
-        this.date = date;
-    }
-
-    public Expense() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public @NotBlank(message = "Title is required") String getTitle() {
-        return title;
-    }
-
-    public void setTitle(@NotBlank(message = "Title is required") String title) {
-        this.title = title;
-    }
-
-    public @NotNull(message = "Amount is Required") @Positive(message = "Amount must be Positive") BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(@NotNull(message = "Amount is Required") @Positive(message = "Amount must be Positive") BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public @NotBlank(message = "Category is required") String getCategory() {
-        return category;
-    }
-
-    public void setCategory(@NotBlank(message = "Category is required") String category) {
-        this.category = category;
-    }
-
-    public @NotNull(message = "Date is required") LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(@NotNull(message = "Date is required") LocalDate date) {
-        this.date = date;
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
